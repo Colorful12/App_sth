c2fcaf4639c3fada5a5a0f4e6ca6a80b1fbed11d
Colorful12 <cat37280@gmail.com>
2021-12-04 02:15:36 +0900

    add .gitignore

--- hello.py
+++ hello.py
@@ -5,4 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+"""
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+"""
 
