2b31051d2c66bd445fb59c36b7b58d910dc2cc8d
Colorful12 <cat37280@gmail.com>
2021-12-07 03:59:06 +0900

    add comments to db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,10 +6,14 @@ from flask.cli import with_appcontext
 
 def get_db():
     if "db" not in g:
+        # "DATABASE"で示されたファイルとのコネクションをつくる. 
+        # そのファイルはこの段階で存在する必要はなく, あとでinitializeするまでその状態は続く.
         g.db = splite3.connect(
+            # currnet_appはdb.pyをインポートしたプログラムを示すってことか？
             current_app.config["DATABASE"],
             detect_types = sqlite3.PARSE_DECLTYPES
         )
+        # 列名での参照ができるようにコネクションを確立.
         get_db.row_factory = splite3.Row
 
     return g.db
