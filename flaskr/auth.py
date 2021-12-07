6c7ccaccc20932a46056db667c2c658a8d37db4e
Colorful12 <cat37280@gmail.com>
2021-12-07 21:27:52 +0900

    fix typo in auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -57,7 +57,7 @@ def login():
 
         if user is None:
             error = "登録されていないユーザーネームが入力されています."
-        elif not check_password_hash(user["password"], password)
+        elif not check_password_hash(user["password"], password):
             error = "パスワードが違います."
         
         if error is None:
