b5adef9d1252368f1966167a133657ac72b8dd78
Colorful12 <cat37280@gmail.com>
2021-12-13 22:57:29 +0900

    create get_post() in blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -48,4 +48,20 @@ def create():
     return render_template("blog/create.html")
 
 
+def get_post(id, check_author=True):
+    # check_author が True になっていることで, この関数がauthorの確認の必要なく利用できる.
+    post = get_db().execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        "FROM post p JOIN user u ON p.author_id = u.id"
+        "WHERE p.id = ?",
+        (id, )
+    ).fetchone()
 
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+    
+    if check_author and post["author_id"] != g.user["id"]:
+        # 403 = アクセス禁止
+        abort(403)
+    
+    return post
