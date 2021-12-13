63d004a611f6314d86a1e2adeccce47f3c730bfd
Colorful12 <cat37280@gmail.com>
2021-12-13 19:52:29 +0900

    add a connection between __init__.py and blog.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,6 +36,13 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint = "index")
+
+    # add_url_rule は, endpoint引数で指定した値とurl_forの引数が一致する場合に
+    # URLを第一引数に変換する. ∴ url_for("index") = "/"
     
 
     return app
