5ef5a57ebe6992f98a013d15062057ef58419ff4
Colorful12 <cat37280@gmail.com>
2021-12-13 19:53:37 +0900

    add index() in blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,5 +6,17 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+# authと違ってurl_prefixがない → indexビューは / , createビューは /createに存在
+# ブログアプリなので / にブログページがあるのは納得.
 bp = Blueprint("blog", __name__)
+
+@bp.route("/")
+def index():
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("blog/index.html", posts=posts)
 
